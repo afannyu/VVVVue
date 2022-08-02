9bf21d53fbd18bbb4a7dab9bcb16f6e8a31f503e
tanguo <3230543061@qq.com>
2022-08-02 15:33:52 +0800

    first commit

---
+++ todolist/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <input v-model="inputValue"/>
+  <button @click="handleClick">增加</button>
+  <ul>
+    <list-item 
+    v-for="item of list" 
+    :key="item"
+    :msg="item" 
+    />
+  </ul>
+</template>
+
+<script>
+import { reactive, ref } from '@vue/reactivity'
+import ListItem from './components/ListItem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ListItem
+  },
+  setup() {
+    const inputValue = ref('')
+    let list = reactive([])
+    const handleClick = ()=>{
+      list.push(inputValue.value)
+      inputValue.value = ''
+    }
+    return {
+      inputValue,list,
+      handleClick
+    }
+  }
+}
+</script>
+
+<style>
+  button {
+    margin-left: 20px;
+    color: red;
+  }
+</style>
